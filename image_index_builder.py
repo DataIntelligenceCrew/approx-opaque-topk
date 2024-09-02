3d945416c96c9822db76f47c94fad1cb0921a166
Jiwon Chang <me@jiwonc.net>
2024-09-01 20:09:33 -0400

    added functionality to unallocated GPU for memory saving

--- image_index_builder.py
+++ image_index_builder.py
@@ -10,6 +10,66 @@ import sys
 import gc
 
 
+def get_less_used_gpu(gpus=None, debug=False):
+    if not torch.cuda.is_available():
+        return None
+    """Inspect cached/reserved and allocated memory on specified gpus and return the id of the less used device"""
+    if gpus is None:
+        warn = 'Falling back to default: all gpus'
+        gpus = range(torch.cuda.device_count())
+    elif isinstance(gpus, str):
+        gpus = [int(el) for el in gpus.split(',')]
+
+    # check gpus arg VS available gpus
+    sys_gpus = list(range(torch.cuda.device_count()))
+    if len(gpus) > len(sys_gpus):
+        gpus = sys_gpus
+        warn = f'WARNING: Specified {len(gpus)} gpus, but only {torch.cuda.device_count()} available. Falling back to default: all gpus.\nIDs:\t{list(gpus)}'
+    elif set(gpus).difference(sys_gpus):
+        # take correctly specified and add as much bad specifications as unused system gpus
+        available_gpus = set(gpus).intersection(sys_gpus)
+        unavailable_gpus = set(gpus).difference(sys_gpus)
+        unused_gpus = set(sys_gpus).difference(gpus)
+        gpus = list(available_gpus) + list(unused_gpus)[:len(unavailable_gpus)]
+        warn = f'GPU ids {unavailable_gpus} not available. Falling back to {len(gpus)} device(s).\nIDs:\t{list(gpus)}'
+
+    cur_allocated_mem = {}
+    cur_cached_mem = {}
+    max_allocated_mem = {}
+    max_cached_mem = {}
+    for i in gpus:
+        cur_allocated_mem[i] = torch.cuda.memory_allocated(i)
+        cur_cached_mem[i] = torch.cuda.memory_reserved(i)
+        max_allocated_mem[i] = torch.cuda.max_memory_allocated(i)
+        max_cached_mem[i] = torch.cuda.max_memory_reserved(i)
+    min_allocated = min(cur_allocated_mem, key=cur_allocated_mem.get)
+    if debug:
+        print(warn)
+        print('Current allocated memory:', {f'cuda:{k}': v for k, v in cur_allocated_mem.items()})
+        print('Current reserved memory:', {f'cuda:{k}': v for k, v in cur_cached_mem.items()})
+        print('Maximum allocated memory:', {f'cuda:{k}': v for k, v in max_allocated_mem.items()})
+        print('Maximum reserved memory:', {f'cuda:{k}': v for k, v in max_cached_mem.items()})
+        print('Suggested GPU:', min_allocated)
+    return min_allocated
+
+
+def free_memory(to_delete: list, debug=False):
+    import gc
+    import inspect
+    calling_namespace = inspect.currentframe().f_back
+    if debug:
+        print('Before:')
+        get_less_used_gpu(debug=True)
+
+    for _var in to_delete:
+        calling_namespace.f_locals.pop(_var, None)
+        gc.collect()
+        torch.cuda.empty_cache()
+    if debug:
+        print('After:')
+        get_less_used_gpu(debug=True)
+
+
 def get_image_vectors_from_directory(directory_name: str, processors: Dict, model: torch.nn.Module, debug_print_: bool) -> List[Tuple[str, np.ndarray]]:
     """
     Given a directory which holds some images, runs the images through a model to get their vector representations.
@@ -35,11 +95,12 @@ def get_image_vectors_from_directory(directory_name: str, processors: Dict, mode
                 vector = features.image_embeds_proj  # Extract low-dimensional feature vector only
                 images_vectors.append((f, vector))
                 debug_print(debug_print_, f"Generated vector for {f}")
-                if iter_ % 1000 == 0:
+                if iter_ % 5000 == 0:
                     gc.collect()
                     torch.cuda.empty_cache()
-                    iter_ = 0
-                iter_ += 1
+                    next_gpu = get_less_used_gpu()
+                    device = torch.device("cuda:" + str(next_gpu) if torch.cuda.is_available() else "cpu")
+            iter_ += 1
     return images_vectors
 
 

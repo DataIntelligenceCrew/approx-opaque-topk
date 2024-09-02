a78253a233659f474c58171b96e8d5f7cf7cdd9f
Jiwon Chang <me@jiwonc.net>
2024-09-02 13:33:31 -0400

    simplified memory free methods

--- image_index_builder.py
+++ image_index_builder.py
@@ -11,64 +11,16 @@ import gc
 import time
 
 
-def get_less_used_gpu(gpus=None, debug=False):
-    if not torch.cuda.is_available():
-        return None
-    """Inspect cached/reserved and allocated memory on specified gpus and return the id of the less used device"""
-    if gpus is None:
-        warn = 'Falling back to default: all gpus'
-        gpus = range(torch.cuda.device_count())
-    elif isinstance(gpus, str):
-        gpus = [int(el) for el in gpus.split(',')]
-
-    # check gpus arg VS available gpus
-    sys_gpus = list(range(torch.cuda.device_count()))
-    if len(gpus) > len(sys_gpus):
-        gpus = sys_gpus
-        warn = f'WARNING: Specified {len(gpus)} gpus, but only {torch.cuda.device_count()} available. Falling back to default: all gpus.\nIDs:\t{list(gpus)}'
-    elif set(gpus).difference(sys_gpus):
-        # take correctly specified and add as much bad specifications as unused system gpus
-        available_gpus = set(gpus).intersection(sys_gpus)
-        unavailable_gpus = set(gpus).difference(sys_gpus)
-        unused_gpus = set(sys_gpus).difference(gpus)
-        gpus = list(available_gpus) + list(unused_gpus)[:len(unavailable_gpus)]
-        warn = f'GPU ids {unavailable_gpus} not available. Falling back to {len(gpus)} device(s).\nIDs:\t{list(gpus)}'
-
-    cur_allocated_mem = {}
-    cur_cached_mem = {}
-    max_allocated_mem = {}
-    max_cached_mem = {}
-    for i in gpus:
-        cur_allocated_mem[i] = torch.cuda.memory_allocated(i)
-        cur_cached_mem[i] = torch.cuda.memory_reserved(i)
-        max_allocated_mem[i] = torch.cuda.max_memory_allocated(i)
-        max_cached_mem[i] = torch.cuda.max_memory_reserved(i)
-    min_allocated = min(cur_allocated_mem, key=cur_allocated_mem.get)
-    if debug:
-        print(warn)
-        print('Current allocated memory:', {f'cuda:{k}': v for k, v in cur_allocated_mem.items()})
-        print('Current reserved memory:', {f'cuda:{k}': v for k, v in cur_cached_mem.items()})
-        print('Maximum allocated memory:', {f'cuda:{k}': v for k, v in max_allocated_mem.items()})
-        print('Maximum reserved memory:', {f'cuda:{k}': v for k, v in max_cached_mem.items()})
-        print('Suggested GPU:', min_allocated)
-    return min_allocated
+def del_vars(to_del: List):
+    for var in to_del:
+        del var
+    gc.collect()
 
 
-def free_memory(to_delete: list, debug=False):
-    import gc
-    import inspect
-    calling_namespace = inspect.currentframe().f_back
-    if debug:
-        print('Before:')
-        get_less_used_gpu(debug=True)
-
-    for _var in to_delete:
-        calling_namespace.f_locals.pop(_var, None)
-        gc.collect()
-        torch.cuda.empty_cache()
-    if debug:
-        print('After:')
-        get_less_used_gpu(debug=True)
+def free_memory(to_free: List):
+    del_vars(to_free)
+    gc.collect()
+    torch.cuda.empty_cache()
 
 
 def get_image_vectors_from_directory(directory_name: str, debug_print_: bool, batch_size: int = 25000) -> List[Tuple[str, np.ndarray]]:
@@ -100,16 +52,9 @@ def get_image_vectors_from_directory(directory_name: str, debug_print_: bool, ba
                 features = model.extract_features({"image": image_tensor, "text_input": ""}, mode="image")
                 vector = features.image_embeds_proj  # Extract low-dimensional feature vector only
                 images_vectors.append((f, vector.cpu().detach().numpy().flatten()))
-                features = None
-                vector = None
-                image = None
-                image_tensor = None
-                path = None
+            del_vars([features, vector, image, image_tensor, path])
             if iter_ % batch_size == batch_size - 1:
-                model = None
-                processors = None
-                gc.collect()
-                torch.cuda.empty_cache()
+                free_memory([model, processors])
             iter_ += 1
     return images_vectors
 

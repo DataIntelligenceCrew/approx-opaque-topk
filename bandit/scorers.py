98fdd493ef1594a30ddebec03b2a9cdc58dafea6
Jiwon Chang <jchang38@ur.rochester.edu>
2024-09-09 17:52:50 -0400

    Added method to get scorer from params.

--- bandit/scorers.py
+++ bandit/scorers.py
@@ -1,5 +1,5 @@
 from datetime import time
-from typing import Dict
+from typing import Dict, Callable
 
 import torch
 import torch.nn as nn
@@ -16,8 +16,17 @@ def relu_score(sample: float, scorer_params: Dict) -> float:
     :return: The score of the sample.
     """
     time.sleep(scorer_params['delay'])
-    return max(0, sample)
+    return max(0.0, sample)
 
 
-
+def get_scorer_from_params(scorer_params: Dict) -> Callable:
+    """
+    Returns the scoring function based on the parameters.
+    :param scorer_params: The parameters for the scoring function.
+    :return: The scoring function.
+    """
+    if scorer_params['type'] == 'relu':
+        return lambda x: relu_score(x, scorer_params)
+    else:
+        raise ValueError(f"Unknown scorer type: {scorer_params['type']}")
 

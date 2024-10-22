b3792cf2d8dfbd7cd4bb11a3eb6bd5ed92a51e78
Jiwon Chang <jchang38@ur.rochester.edu>
2024-10-22 12:54:19 -0400

    Cleaned up run_expr

--- run_expr.py
+++ run_expr.py
@@ -1,5 +1,5 @@
+import argparse
 import json
-import sys
 
 import numpy as np
 
@@ -7,84 +7,44 @@ from bandit.bandit import approx_top_k_bandit
 from bandit.sampler import get_sampler_from_params
 from bandit.scorers import get_scorer_from_params
 
-gt_config_name = "Scan"
 
 """
 Runs an experiment with a config file as defined in expr_json_spec.
-For each config in the experiment, computes the following statistics, as mean, per iteration. 
- - STK
- - KLS
- - Time
- - Precision@K
- - Recall@K
- - Average ranking
- - Kth ranking
+This should be run after the gt experiments, as it uses the gt statistics to compute metrics. 
 
-In order to do so, we assume that the first config is the ground truth (exhaustive search), ran only once. 
-
-USAGE: python3 bandit_expr.py <config_file> <output_file>
+USAGE: python run_expr.py <config_filename> <gt_result_filename> <output_filename>
 """
 if __name__ == '__main__':
-    config_stats = {}
-
     # Parse arguments
-    config_filename = sys.argv[1]
-    output_file = sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_filename", type=str, help="Path to the configuration file")
+    parser.add_argument("gt_filename", type=str, help="Path to the ground truth result file")
+    parser.add_argument("output_filename", type=str, help="Path to the output file")
+    args = parser.parse_args()
 
     # Import config
-    with open(config_filename, 'r') as file:
+    with open(args.config_filename, 'r') as file:
         all_configs = json.load(file)
 
-    # Run first experiment as ground truth
-    gt_config = all_configs[gt_config_name]
-    gt_k = gt_config['k']
-    gt_result = approx_top_k_bandit(
-        index_filename=gt_config['index'],
-        k=gt_k,
-        scoring_fn=get_scorer_from_params(gt_config['scoring_params']),
-        scoring_params=gt_config['scoring_params'],
-        sampling_fn=get_sampler_from_params(gt_config['sampling_params']),
-        sampling_params=gt_config['sampling_params'],
-        algorithm=gt_config['algo_params']['type'],
-        algo_params=gt_config['algo_params'],
-        budget=gt_config['budget'],
-        sample_method=gt_config['sample_method']
-    )
-    print("Ran GT")
-    # Sort sample IDs in descending order
-    scores_and_keys = [(iter_result['score'], iter_result['sample_id']) for iter_result in gt_result]
-    scores_and_keys.sort(reverse=True)
-    gt_max_rank = len(scores_and_keys)+1
-    # Compute the gt statistics
-    gt_id_to_ranking = {}
-    for idx, (score, key) in enumerate(scores_and_keys):
-        gt_id_to_ranking[key] = idx+1
-    # Keep aside the gt solution
-    gt_solution = set(gt_result[-1]['pq_elements'])
-    # Add gt aggregate statistics to stats
-    config_stats[gt_config_name] = {
-        "STK": np.array([iter_result['stk'] for iter_result in gt_result]),
-        "KLS": np.array([iter_result['kls'] for iter_result in gt_result]),
-        "time": np.array([iter_result['time'] for iter_result in gt_result]),
-        "precision": np.array([len([elem for elem in iter_result['pq_elements'] if elem in gt_solution])
-                      for iter_result in gt_result]) / gt_k,
-        "recall": np.array([len([elem for elem in gt_solution if elem in iter_result['pq_elements']])
-                   for iter_result in gt_result]) / gt_k,
-        "avg_rank": np.array([sum([gt_id_to_ranking[iter_result['pq_elements'][k_]]
-                                   if k_ < len(iter_result['pq_elements']) else gt_max_rank for k_ in range(gt_k)])
-                              for iter_result in gt_result]) / gt_k,
-        "kth_rank": np.array([float(gt_id_to_ranking[iter_result['pq_elements'][gt_k-1]])
-                              if gt_k <= len(iter_result['pq_elements']) else gt_max_rank for iter_result in gt_result])
-    }
+    # Import gt results
+    with open(args.gt_filename, 'r') as file:
+        gt_result = json.load(file)
+    gt_solution = set(gt_result['gt_solution'])  # The ground truth solution set
+    gt_rankings = gt_result['gt_rankings']  # A mapping from element ID to ground truth ranking
+    gt_max_rank = gt_result['n']
+    gt_k = len(gt_solution)
+
+    # A dictionary containing all configs' results
+    config_stats = dict()
 
     for expr_name, expr_config in all_configs.items():  # Run all remaining experiments
-        if expr_name == gt_config_name:
-            continue
         expr_reps = expr_config['reps']
         expr_budget = expr_config['budget']
+
         for rep in range(expr_reps):  # Repeat for specified number of runs
+            # Run bandit
             run_result = approx_top_k_bandit(
-                index_filename=expr_config['index'],
+                index_params=expr_config['index_params'],
                 k=expr_config['k'],
                 scoring_fn=get_scorer_from_params(expr_config['scoring_params']),
                 scoring_params=expr_config['scoring_params'],
@@ -93,53 +53,55 @@ if __name__ == '__main__':
                 algorithm=expr_config['algo_params']['type'],
                 algo_params=expr_config['algo_params'],
                 budget=expr_budget,
-                sample_method=expr_config['sample_method']
+                sample_method=expr_config['sample_method'],
+                batch_size=expr_config['batch_size'],
+                gt_rankings=gt_rankings,
+                gt_solution=gt_solution
             )
-            # Compute statistics for this run
-            iter_stks = np.array([iter_result['stk'] for iter_result in run_result])
-            iter_klss = np.array([iter_result['kls'] for iter_result in run_result])
-            iter_times = np.array([iter_result['time'] for iter_result in run_result])
-            iter_precisions = np.array([len([elem for elem in iter_result['pq_elements'] if elem in gt_solution])
-                               for iter_result in run_result]) / gt_k
-            iter_recalls = np.array([len([elem for elem in gt_solution if elem in iter_result['pq_elements']])
-                                     for iter_result in run_result]) / gt_k
-            iter_avg_ranks = np.array([sum([gt_id_to_ranking[iter_result['pq_elements'][k_]]
-                                   if k_ < len(iter_result['pq_elements']) else gt_max_rank for k_ in range(gt_k)])
-                              for iter_result in run_result]) / gt_k
-            iter_kth_ranks = np.array([float(gt_id_to_ranking[iter_result['pq_elements'][gt_k-1]])
-                              if gt_k <= len(iter_result['pq_elements']) else gt_max_rank for iter_result in run_result])
+
+            # Compute metrics
+            rep_stks = np.array([x['STK'] for x in run_result['iter_results']])
+            rep_klss = np.array([x['KLS'] for x in run_result['iter_results']])
+            rep_times = np.array([x['time'] for x in run_result['iter_results']])
+            rep_precisions = np.array([x['Precision@K'] for x in run_result['iter_results']])
+            rep_recalls = np.array([x['Recall@K'] for x in run_result['iter_results']])
+            rep_avg_ranks = np.array([x['AvgRank'] for x in run_result['iter_results']])
+            rep_worst_ranks = np.array([x['WorstRank'] for x in run_result['iter_results']])
+
             # Create dictionary entry if not exists
             if rep == 0:
                 config_stats[expr_name] = {
-                    "STK": iter_stks,
-                    "KLS": iter_klss,
-                    "time": iter_times,
-                    "precision": iter_precisions,
-                    "recall": iter_recalls,
-                    "avg_rank": iter_avg_ranks,
-                    "kth_rank": iter_kth_ranks
+                    "STK": rep_stks,
+                    "KLS": rep_klss,
+                    "time": rep_times,
+                    "Precision@K": rep_precisions,
+                    "Recall@K": rep_recalls,
+                    "AvgRank": rep_avg_ranks,
+                    "WorstRank": rep_worst_ranks,
+                    "reps": expr_reps
                 }
             else:  # Otherwise, sum all metrics
-                config_stats[expr_name]["STK"] += iter_stks
-                config_stats[expr_name]["KLS"] += iter_klss
-                config_stats[expr_name]["time"] += iter_times
-                config_stats[expr_name]["precision"] += iter_precisions
-                config_stats[expr_name]["recall"] += iter_recalls
-                config_stats[expr_name]["avg_rank"] += iter_avg_ranks
-                config_stats[expr_name]["kth_rank"] += iter_kth_ranks
+                config_stats[expr_name]["STK"] += rep_stks
+                config_stats[expr_name]["KLS"] += rep_klss
+                config_stats[expr_name]["time"] += rep_times
+                config_stats[expr_name]["Precision@K"] += rep_precisions
+                config_stats[expr_name]["Recall@K"] += rep_recalls
+                config_stats[expr_name]["AvgRank"] += rep_avg_ranks
+                config_stats[expr_name]["WorstRank"] += rep_worst_ranks
+
             print("Completed", expr_name, "rep", rep)
 
     # Average out all metrics, then convert numpy arrays to list for serialization
     for expr_name, expr_results in config_stats.items():
-        expr_reps = all_configs[expr_name]['reps']
+        expr_reps = expr_results['reps']
         config_stats[expr_name]["STK"] = list(config_stats[expr_name]["STK"] / float(expr_reps))
         config_stats[expr_name]["KLS"] = list(config_stats[expr_name]["KLS"] / float(expr_reps))
         config_stats[expr_name]["time"] = list(config_stats[expr_name]["time"] / float(expr_reps))
-        config_stats[expr_name]["precision"] = list(config_stats[expr_name]["precision"] / float(expr_reps))
-        config_stats[expr_name]["recall"] = list(config_stats[expr_name]["recall"] / float(expr_reps))
-        config_stats[expr_name]["avg_rank"] = list(config_stats[expr_name]["avg_rank"] / float(expr_reps))
-        config_stats[expr_name]["kth_rank"] = list(config_stats[expr_name]["kth_rank"] / float(expr_reps))
+        config_stats[expr_name]["Precision@K"] = list(config_stats[expr_name]["Precision@K"] / float(expr_reps))
+        config_stats[expr_name]["Recall@K"] = list(config_stats[expr_name]["Recall@K"] / float(expr_reps))
+        config_stats[expr_name]["AvgRank"] = list(config_stats[expr_name]["AvgRank"] / float(expr_reps))
+        config_stats[expr_name]["WorstRank"] = list(config_stats[expr_name]["WorstRank"] / float(expr_reps))
 
     # Save results
-    with open(output_file, 'w') as file:
+    with open(args.output_filename, 'w') as file:
         json.dump(config_stats, file, indent=2)
--- subsample_cars.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/localdisk2/approx-topk/Data/UsedCars/val.csv')
-df = df.head(200000)
-
-df.to_csv('/localdisk2/approx-topk/Data/UsedCars/subset.csv', index=False)
